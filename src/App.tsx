930d0b5fdcb5d2519b48a990767650548096eda9
JoinQuit <hn58013@gmail.com>
2025-08-29 18:53:00 +0700

    up date

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import reactLogo from './assets/whale-removebg-preview.png'
-import TypingEffect from './component/TypingEffect' // Import component TypingEffect
-import './App.css'
+import TypingEffect from './component/TypingEffect'
 
 function App() {
+  const text1 = `🌸 Card 1
+Chào em! Đây là card đầu tiên.
+Nội dung sẽ hiện dần như đang gõ...
+Yêu thương lắm!`
+
+  const text2 = `🌻 Card 2
+Đây là card thứ hai nè!
+Mình có thể để nội dung khác ở đây.
+Vuốt qua lại để xem các card nhé 💖`
+
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'></a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className="w-screen h-screen overflow-x-scroll flex snap-x snap-mandatory scroll-smooth">
+      {/* Card 1 */}
+      <div className="w-screen h-full flex-shrink-0 snap-start p-6 bg-white text-gray-800">
+        <div className="bg-pink-100 shadow-lg rounded-xl p-6 h-full overflow-y-auto">
+          <TypingEffect text={text1} />
+        </div>
       </div>
-      <TypingEffect />
-    </>
+
+      {/* Card 2 */}
+      <div className="w-screen h-full flex-shrink-0 snap-start p-6 bg-white text-gray-800">
+        <div className="bg-yellow-100 shadow-lg rounded-xl p-6 h-full overflow-y-auto">
+          <TypingEffect text={text2} />
+        </div>
+      </div>
+    </div>
   )
 }
 
